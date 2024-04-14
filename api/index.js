31cdb972387fd5879c6c0bd26ac96663650ad26c
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-04-14 18:40:20 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import colors from "colors";
+import dotenv from "dotenv"
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`MongoDB is Connected`.bgMagenta.white);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const PORT = 5000;
 
-app.listen(PORT, ()=> {
-    console.log(`Server Running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`.bgCyan.white);
+});
