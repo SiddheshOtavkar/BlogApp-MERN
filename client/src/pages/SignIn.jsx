530b8381be86b6f95f18be87f7f7787791549a6b
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-04-17 10:08:24 +0530

    done with singin functions

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -100,7 +100,7 @@ const SignIn = () => {
                     </form>
                     <div className='flex gap-2 text-sm mt-5'>
                         <span>Dont Have an account?</span>
-                        <Link to='/sign-up' className='text-blue-500'>
+                        <Link to='/sign-up' className='text-blue-500 hover:underline'>
                             Sign Up
                         </Link>
                     </div>
