6374c04d4d3c8a7195a86a13ac91d468cb562b45
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-04-19 10:35:33 +0530

    added dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,23 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 const Header = () => {
     const path = useLocation().pathname;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
+    const { theme } = useSelector((state) => state.theme);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const handleSignout = async () => {
         console.log("hello");
-    }
+    };
 
     return (
         <Navbar className="border-b-2">
@@ -36,24 +42,29 @@ const Header = () => {
             </Button>
 
             <div className="flex gap-2 md:order-2">
-                <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-                    <FaMoon />
+                <Button
+                    className="w-10 h-9 sm:w-12 sm:h-10 sm:inline"
+                    color="gray"
+                    pill
+                    onClick={() => dispatch(toggleTheme())}
+                >
+                    {theme === "light" ? <FaSun /> : <FaMoon />}
                 </Button>
                 {currentUser ? (
                     <Dropdown
                         arrowIcon={false}
                         inline
                         label={
-                            <Avatar alt='user' img={currentUser.profilePicture} rounded />
+                            <Avatar alt="user" img={currentUser.profilePicture} rounded />
                         }
                     >
                         <Dropdown.Header>
-                            <span className='block text-sm'>@{currentUser.username}</span>
-                            <span className='block text-sm font-medium truncate'>
+                            <span className="block text-sm">@{currentUser.username}</span>
+                            <span className="block text-sm font-medium truncate">
                                 {currentUser.email}
                             </span>
                         </Dropdown.Header>
-                        <Link to={'/dashboard?tab=profile'}>
+                        <Link to={"/dashboard?tab=profile"}>
                             <Dropdown.Item>Profile</Dropdown.Item>
                         </Link>
                         <Dropdown.Divider />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,14 @@ import './index.css'
 import { persistor, store } from './redux/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-// import ThemeProvider from './components/ThemeProvider.jsx';
+import ThemeProvider from './components/ThemeProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      {/* <ThemeProvider> */}
+      <ThemeProvider>
         <App />
-      {/* </ThemeProvider> */}
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 )
