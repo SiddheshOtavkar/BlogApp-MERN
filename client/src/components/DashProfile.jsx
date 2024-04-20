b672d3800737108222c3029b73a8c0a01e5b8295
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-04-20 13:17:50 +0530

    image2

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,76 +1,113 @@
-import { Button, TextInput } from 'flowbite-react';
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Button, TextInput } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from './../firbase';
 
 const DashProfile = () => {
-
     const { currentUser } = useSelector((state) => state.user);
-    // console.log(currentUser);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
     const [formData, setFormData] = useState({});
+    const filePickerRef = useRef();
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+    };
+    console.log(imageFile, imageFileUrl);
+
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile]);
+
+    const uploadImage = async () => {
+        const storage = getStorage(app);
     }
 
     return (
-        <div className='max-w-lg mx-auto p-3 w-full'>
-            <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-            <form className='flex flex-col gap-4'>
+        <div className="max-w-lg mx-auto p-3 w-full">
+            <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+            <form className="flex flex-col gap-4">
+                <input
+                    type="file"
+                    accept="image/*"
+                    hidden
+                    onChange={handleImageChange}
+                    ref={filePickerRef}
+                />
                 <div
-                    className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+                    className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+                    onClick={() => filePickerRef.current.click()}
                 >
                     <img
-                        src={currentUser.profilePicture}
+                        src={imageFileUrl || currentUser.profilePicture}
                         alt="user"
                         className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] `}
                     />
                 </div>
                 <TextInput
-                    type='text'
-                    id='username'
-                    placeholder='username'
+                    type="text"
+                    id="username"
+                    placeholder="username"
                     defaultValue={currentUser.username}
                     onChange={handleChange}
                 />
                 <TextInput
-                    type='email'
-                    id='email'
-                    placeholder='email'
+                    type="email"
+                    id="email"
+                    placeholder="email"
                     defaultValue={currentUser.email}
                     onChange={handleChange}
                 />
                 <TextInput
-                    type='password'
-                    id='password'
-                    placeholder='password'
+                    type="password"
+                    id="password"
+                    placeholder="password"
                     onChange={handleChange}
                 />
-                <Button
-                    type='submit'
-                    gradientDuoTone='purpleToBlue'
-                    outline
-                >
+                <Button type="submit" gradientDuoTone="purpleToBlue" outline>
                     Update
                 </Button>
                 {currentUser.isAdmin && (
-                    <Link to={'/create-post'}>
+                    <Link to={"/create-post"}>
                         <Button
-                            type='button'
-                            gradientDuoTone='purpleToPink'
-                            className='w-full'
+                            type="button"
+                            gradientDuoTone="purpleToPink"
+                            className="w-full"
                         >
                             Create a post
                         </Button>
                     </Link>
                 )}
             </form>
-            <div className='text-red-500 flex justify-between mt-5'>
-                <span className='cursor-pointer'>Delete Account</span>
-                <span className='cursor-pointer'>Sign Out</span>
+            <div className="text-red-500 flex justify-between mt-5">
+                <span className="cursor-pointer">Delete Account</span>
+                <span className="cursor-pointer">Sign Out</span>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default DashProfile
+export default DashProfile;
