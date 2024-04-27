35cfadbf9ecbf1d5d51614a53452dc20d43c5270
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-04-27 20:18:36 +0530

    delete user frontend done

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -11,7 +11,15 @@ import {
 import { app } from "./../firbase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { updateFailure, updateStart, updateSuccess } from "../redux/user/userSlice";
+import {
+    deleteUserFailure,
+    deleteUserStart,
+    deleteUserSuccess,
+    updateFailure,
+    updateStart,
+    updateSuccess,
+} from "../redux/user/userSlice";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 const DashProfile = () => {
     const { currentUser } = useSelector((state) => state.user);
@@ -89,19 +97,19 @@ const DashProfile = () => {
         setUpdateUserSuccess(null);
 
         if (Object.keys(formData).length === 0) {
-            setUpdateUserError('No changes made');
+            setUpdateUserError("No changes made");
             return;
         }
         if (imageFileUploading) {
-            setUpdateUserError('Please wait for image to upload');
+            setUpdateUserError("Please wait for image to upload");
             return;
         }
         try {
             dispatch(updateStart());
             const res = await fetch(`/api/user/update/${currentUser._id}`, {
-                method: 'PUT',
+                method: "PUT",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify(formData),
             });
@@ -117,6 +125,25 @@ const DashProfile = () => {
         } catch (error) {
             dispatch(updateFailure(error.message));
             setUpdateUserError(error.message);
+        }
+    };
+
+    const handleDeleteUser = async () => {
+        setShowModal(false);
+        try {
+            dispatch(deleteUserStart());
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+
+            if (!res.ok) {
+                dispatch(deleteUserFailure(data.message));
+            } else {
+                dispatch(deleteUserSuccess(data));
+            }
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message));
         }
     };
 
@@ -138,7 +165,11 @@ const DashProfile = () => {
                     {imageFileUploadProgress && (
                         <CircularProgressbar
                             value={imageFileUploadProgress || 0}
-                            text={imageFileUploadProgress < 100 ? `${imageFileUploadProgress}%` : ""}
+                            text={
+                                imageFileUploadProgress < 100
+                                    ? `${imageFileUploadProgress}%`
+                                    : ""
+                            }
                             strokeWidth={5}
                             styles={{
                                 root: {
@@ -149,7 +180,8 @@ const DashProfile = () => {
                                     left: 0,
                                 },
                                 path: {
-                                    stroke: `rgba(62, 152, 199, ${imageFileUploadProgress / 100})`,
+                                    stroke: `rgba(62, 152, 199, ${imageFileUploadProgress / 100
+                                        })`,
                                 },
                             }}
                         />
@@ -203,19 +235,45 @@ const DashProfile = () => {
                 )}
             </form>
             <div className="text-red-500 flex justify-between mt-5">
-                <span className="cursor-pointer">Delete Account</span>
+                <span onClick={() => setShowModal(true)} className="cursor-pointer">
+                    Delete Account
+                </span>
                 <span className="cursor-pointer">Sign Out</span>
             </div>
             {updateUserSuccess && (
-                <Alert color='success' className='mt-5'>
+                <Alert color="success" className="mt-5">
                     {updateUserSuccess}
                 </Alert>
             )}
             {updateUserError && (
-                <Alert color='failure' className='mt-5'>
+                <Alert color="failure" className="mt-5">
                     {updateUserError}
                 </Alert>
             )}
+            <Modal
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                popup
+                size="md"
+            >
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                        <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                            Are you sure you want to delete your account?
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color='failure' onClick={handleDeleteUser}>
+                                Yes, I'm sure
+                            </Button>
+                            <Button color='gray' onClick={() => setShowModal(false)}>
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     );
 };
