9741bfe9196522974795b99750ae79bc453fbf62
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-06-17 21:19:22 +0530

    Complete post functinality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
         </Route>
 
         <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
       </Routes>
       <FooterComm />
     </Router>
