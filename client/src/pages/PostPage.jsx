a450b37f3f034abba0b656db6e4f98e832e1efd4
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-06-24 18:10:32 +0530

    Comment section part 1

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,8 +2,8 @@ import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import PostCard from './../components/PostCard';
-import CommentSection from './../components/CommentSection';
 import CallToAction from './../components/CallToAction';
+import CommentSection from '../components/CommentSection';
 
 const PostPage = () => {
     const { postSlug } = useParams();
