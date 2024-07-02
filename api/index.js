cafc2f6b86851df90894f373ea01e2ed8c15ba5e
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-07-02 12:33:44 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ import postRoutes from "./routes/post.route.js";
 import commentRoutes from './routes/comment.route.js';
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 const app = express();
 app.use(express.json());
@@ -24,6 +25,8 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const PORT = process.env.port || 5000;
 
 app.use('/api/user', userRoutes);
@@ -31,10 +34,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  
+
   res.status(statusCode).json({
     success: false,
     statusCode,
